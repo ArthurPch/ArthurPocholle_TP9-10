fbfb578665db6e3b7f9ed63010ad46f395b66c17
Arthur <arthur.pocholle@gmail.com>
2025-03-18 11:31:37 +0100

    Partie 1: Fin de l'arborescence du tp9+ creation du pdo.php+importation de celui ci dans l'index.php

---
+++ exo-tp/index.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'Model/pdo.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 9 - Manipulation SQL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Exercice 9 - Manipulation SQL</h1>
+        
+        
+    </div>
+</body>
+</html>
